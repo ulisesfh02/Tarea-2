c9e70209e99e8ff1145ef6d3fa39402d966ca5b5
JOSE ULISES FONSECA HURTADO <jose.fonsecahurtado@ucr.ac.cr>
2022-05-17 22:59:50 -0600

    Se agregan metodos de Planilla.h en Planilla.cpp

---
+++ src/Planilla.cpp
@@ -0,0 +1,12 @@
+#include "Planilla.h"
+
+Planilla::Planilla(){
+}
+Planilla::~Planilla(){
+}
+void Planilla::agregarEmpleado(TipoEmpleado * empleado){
+}
+istream& operator >> (istream &i, Planilla *planilla){
+}
+ostream& operator << (ostream &i, const Planilla *planilla){
+}
