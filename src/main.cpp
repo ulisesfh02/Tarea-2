0061757dad9062b068e6b8fd56df3f81d23d789c
JOSE ULISES FONSECA HURTADO <jose.fonsecahurtado@ucr.ac.cr>
2022-05-17 22:24:21 -0600

    Se agrega clase main donde se hara la lectura de datos

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "TipoEmpleado.h"
+#include "EmpleadoNomina.h"
+#include "ProfesionalHoras.h"
+#include "Planilla.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
