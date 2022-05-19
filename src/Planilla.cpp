c06b0bff202fe0bc43b84f92984a5dc1c813f983
JOSE ULISES FONSECA HURTADO <jose.fonsecahurtado@ucr.ac.cr>
2022-05-18 23:20:41 -0600

    Se agrega principio de Planilla, pendiente a cambiar

--- src/Planilla.cpp
+++ src/Planilla.cpp
@@ -1,11 +1,21 @@
 #include "Planilla.h"
+#include <iostream>
+#include <sstream>
+#include <string>
 
 Planilla::Planilla(){
 }
+
 Planilla::~Planilla(){
+    for(TipoEmpleado* empleado: this->empleados){
+        delete empleado;
+    }
 }
+
 void Planilla::agregarEmpleado(TipoEmpleado * empleado){
+    this->empleados.push_back(empleado);
 }
+
 istream& operator >> (istream &i, Planilla *planilla){
 }
 ostream& operator << (ostream &i, const Planilla *planilla){
