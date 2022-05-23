e14dd656602de8badee307c3b3a83476e0cb756e
ulisesfh02 <jose.fonsecahurtado@ucr.ac.cr>
2022-05-22 22:00:49 -0600

    Se agregan cambios en los tests

--- tests/tests_empleadoNomina.cpp
+++ tests/tests_empleadoNomina.cpp
@@ -1,6 +1,24 @@
 #include <gtest/gtest.h>
 #include "../src/EmpleadoNomina.h"
+#include "../src/Planilla.h"
 
 namespace{
-    
+    TEST(Test_EmpleadoNomina, Test_Agregar_EmpleadoNomina){
+        /// AAA
+
+        // Arrange - configurar el escenario
+        Planilla *planilla = new Planilla();
+        EmpleadoNomina *empleado = new EmpleadoNomina();
+        // Act - ejecute la operación
+        planilla->agregarEmpleado(empleado);
+
+        int actual = planilla->obtenerIdEmpleado(2);
+        int esperada = 2;
+
+        delete planilla;
+        delete empleado;
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
 }
--- tests/tests_profesionalHoras.cpp
+++ tests/tests_profesionalHoras.cpp
@@ -1,5 +1,24 @@
 #include <gtest/gtest.h>
 #include "../src/ProfesionalHoras.h"
+#include "../src/Planilla.h"
 
 namespace{
+    TEST(Test_ProfesionalHoras, Test_Agregar_ProfesionalHoras){
+        /// AAA
+
+        // Arrange - configurar el escenario
+        Planilla *planilla = new Planilla();
+        ProfesionalHoras *empleado = new ProfesionalHoras();
+        // Act - ejecute la operación
+        planilla->agregarEmpleado(empleado);
+
+        int actual = planilla->obtenerIdEmpleado(2);
+        int esperada = 2;
+
+        delete planilla;
+        delete empleado;
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
 }
